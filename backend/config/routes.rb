074ac9014de2861f1a4995cd299fc75f729f967f
Johnny Squardov <johnny@johnmichael.dev>
2021-02-07 11:21:46 -0500

    Setup create, login, and auto_login routes

---
+++ backend/config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resource :users, only: [:create]
+  post "/create", to: "users#create"
+  post "/login", to: "users#login"
+  get "/auto_login", to: "users#auto_login"
+end
