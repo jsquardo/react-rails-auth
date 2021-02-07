d55e0549a44d8bd671bcd249edc808a6076af6df
Johnny Squardov <johnny@johnmichael.dev>
2021-02-07 11:43:30 -0500

    Fixed login error

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -14,9 +14,9 @@ class UsersController < ApplicationController
 
   # LOGGIN IN
   def login
-    @user = User.find_by(username: params[:password])
+    @user = User.find_by(username: params[:username])
 
-    if @user && @user.authentication(params[:password])
+    if @user && @user.authenticate(params[:password])
       token = encode_token({ user_id: @user.id })
       render json: { user: @user, token: token }
     else
